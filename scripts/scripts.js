484d459df4c6992980acdf7c78d052ca517ba270
Lians <>
2026-04-22 20:44:05 -0500

    Portafolio responsive y configuraciones de pet

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
-$(document).ready(function(){
-    $(".left-nav").hide();
-})
+var hbtn  = document.querySelector('.hbtn');
+hbtn.addEventListener('click',function() {
+    hbtn.classList.toggle('hbtn-open');
+});
