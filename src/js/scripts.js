162a32ce2280882145f43b6c45cb205d9ea49f2c
Lians <>
2026-04-24 19:05:15 -0500

    Adicción de responsive al sitio de mascotas

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,3 +4,25 @@ document.querySelector('.button').addEventListener('click', function () {
     console.log(document.querySelector('.left-container').classList);
     this.classList.toggle('active');
 });
+
+const welcomeEl = document.getElementById("welcome-text")
+welcomeEl.innerText = "Welcome to Soft Paws! Try our new black mode option!";
+
+setTimeout(() => {
+    welcomeEl.style.opacity = "0";
+}, 6000);
+
+window.onload = () => {
+    document.getElementById("welcome-text").style.opacity = "1";
+  };
+
+const btn = document.getElementById('color-toggle')
+
+btn.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        btn.textContent = "Light mode";
+    } else {
+        btn.textContent = "Dark mode";
+    }
+});
